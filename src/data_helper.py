5083f70c886e13b9e3efb471fdecd198d9c312eb
Tuatini Godard <tuatinigodard@gmail.com>
2017-05-09 18:04:36 +0200

    Add opencv normalization

--- src/data_helper.py
+++ src/data_helper.py
@@ -1,12 +1,12 @@
 import os
 import sys
 import cv2
+import gc
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from itertools import chain
 from multiprocessing import Pool, cpu_count
-
 
 def get_jpeg_data_files_paths():
     """
@@ -28,9 +28,15 @@ def get_jpeg_data_files_paths():
     assert os.path.exists(train_csv_file), "The {} file does not exist".format(train_csv_file)
     return [train_jpeg_dir, test_jpeg_dir, test_jpeg_additional, train_csv_file]
 
+def free_memory(pool):
+    pool.close()
+    pool.join()
+    gc.collect()
 
 def _train_transform_to_matrices(file_path, tags, labels_map, img_resize):
-    img_array = cv2.resize(cv2.imread(file_path), img_resize)
+    img_array = np.array(cv2.resize(cv2.imread(file_path), img_resize), dtype=np.float32, copy=False)
+    cv2.normalize(img_array, img_array, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
     targets = np.zeros(len(labels_map))
     for t in tags.split(' '):
         targets[labels_map[t]] = 1
@@ -38,11 +44,14 @@ def _train_transform_to_matrices(file_path, tags, labels_map, img_resize):
 
 
 def _test_transform_to_matrices(test_set_folder, file_name, img_resize):
-    img_array = cv2.resize(cv2.imread('{}/{}'.format(test_set_folder, file_name)), img_resize)
+    img_array = np.array(cv2.resize(cv2.imread('{}/{}'.format(test_set_folder, file_name)), img_resize),
+                         dtype=np.float32, copy=False)
+    # Maybe remove this one
+    cv2.normalize(img_array, img_array, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
     return [img_array, file_name]
 
 
-def _get_train_matrices(train_set_folder, train_csv_file, scale_fct, img_resize):
+def _get_train_matrices(train_set_folder, train_csv_file, img_resize, process_count):
     labels_df = pd.read_csv(train_csv_file)
     labels = sorted(set(chain.from_iterable([tags.split(" ") for tags in labels_df['tags'].values])))
     labels_map = {l: i for i, l in enumerate(labels)}
@@ -56,44 +65,46 @@ def _get_train_matrices(train_set_folder, train_csv_file, scale_fct, img_resize)
     # Multiprocess transformation
     x_train = []
     y_train = []
-    cpu_n = cpu_count()
-    pool = Pool(cpu_n)
-    print("Transforming train data to matrices. Using {} threads...".format(cpu_n))
+    pool = Pool(process_count)
+    print("Transforming train data to matrices. Using {} threads...".format(process_count))
     sys.stdout.flush()
     for img_array, targets in pool.starmap(_train_transform_to_matrices,
                                            ((file_path, tag, labels_map, img_resize)
                                             for file_path, tag in zip(files_path, tags_list))):
         x_train.append(img_array)
         y_train.append(targets)
+    free_memory(pool)
+    return [np.array(x_train), np.array(y_train, np.uint8), {v: k for k, v in labels_map.items()}]
 
-    x_train = scale_fct(np.array(x_train, np.float32))
-    return [x_train, np.array(y_train, np.uint8), {v: k for k, v in labels_map.items()}]
 
-
-def _get_test_matrices(test_set_folder, test_set_additional, img_resize):
+def _get_test_matrices(test_set_folder, test_set_additional, img_resize, process_count):
     x_test = []
     x_test_filename = []
-    cpu_n = cpu_count()
-    pool = Pool(cpu_n)
+    pool = Pool(process_count)
     files_name = os.listdir(test_set_folder)
     additional_files_name = os.listdir(test_set_additional)
-    print("Transforming test data to matrices. Using {} threads...".format(cpu_n))
+    print("Transforming test data to matrices. Using {} threads...".format(process_count))
     sys.stdout.flush()
     for img_array, file_name in pool.starmap(_test_transform_to_matrices, ((test_set_folder, file_name, img_resize)
                                                                            for file_name in files_name)):
         x_test.append(img_array)
         x_test_filename.append(file_name)
 
-    print("Transforming additional test data to matrices. Using {} threads...".format(cpu_n))
+    free_memory(pool)
+    pool = Pool(process_count)
+    print("Transforming additional test data to matrices. Using {} threads...".format(process_count))
     sys.stdout.flush()
     for img_array, file_name in pool.starmap(_test_transform_to_matrices, ((test_set_additional, file_name, img_resize)
                                                                            for file_name in additional_files_name)):
         x_test.append(img_array)
         x_test_filename.append(file_name)
+
+    free_memory(pool)
     return [np.array(x_test, np.float32), np.array(x_test_filename)]
 
 
-def preprocess_data(train_set_folder, test_set_folder, test_set_additional, train_csv_file, img_resize=(32, 32)):
+def preprocess_data(train_set_folder, test_set_folder,
+                    test_set_additional, train_csv_file, img_resize=(32, 32), process_count=cpu_count()):
     """
     Transform the images to ready to use data for CNN
     :param train_set_folder: the folder containing the images for training
@@ -102,6 +113,9 @@ def preprocess_data(train_set_folder, test_set_folder, test_set_additional, trai
             https://www.kaggle.com/c/planet-understanding-the-amazon-from-space/discussion/32157
     :param train_csv_file: the file containing the labels of the training images
     :param img_resize: the standard size you want to have on images when transformed to matrices
+    :param process_count: the number of process you want to use to preprocess the data.
+        The more you use, the faster it will be but depending on your RAM you may get a MemoryError.
+        If you do, lower this number. Its default value is equal to the number of core of your CPU
     :return: The images matrices and labels as [x_train, x_test, y_train, labels_map, x_test_filename]
         x_train: The X train values as a numpy array
         x_test: The X test values as a numpy array
@@ -109,7 +123,7 @@ def preprocess_data(train_set_folder, test_set_folder, test_set_additional, trai
         labels_map: The mapping between the tags labels and their indices
         x_test_filename: The files name of each test images in the same order as the x_test arrays
     """
-    x_train, y_train, labels_map = _get_train_matrices(train_set_folder, train_csv_file, lambda x: x / 255, img_resize)
-    x_test, x_test_filename = _get_test_matrices(test_set_folder, test_set_additional, img_resize)
+    x_train, y_train, labels_map = _get_train_matrices(train_set_folder, train_csv_file, img_resize, process_count)
+    x_test, x_test_filename = _get_test_matrices(test_set_folder, test_set_additional, img_resize, process_count)
     print("Done.")
     return [x_train, x_test, y_train, labels_map, x_test_filename]
