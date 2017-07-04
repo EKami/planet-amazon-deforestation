607c0e564d3f48adc31f87cb0551379548d701c4
Tuatini Godard <tuatinigodard@gmail.com>
2017-07-04 18:01:27 +0200

    Fix architecture

--- src/gans.py
+++ src/gans.py
@@ -6,6 +6,12 @@ import data_helper
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers.convolutional import UpSampling2D
+from keras.layers.convolutional import Conv2DTranspose
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import RMSprop
+from keras.layers.core import Reshape
 
 
 class Gans:
@@ -15,76 +21,80 @@ class Gans:
         self.discriminator_model = Sequential()
         self.adversarial_model = Sequential()
         self.preprocessor = preprocessor
+        self.img_channels = 3
 
     def is_images_already_generated(self):
         return False
 
-    def add_discriminator(self):
-        depth = 64
+    def add_discriminator(self, output_size):
         dropout = 0.4
-        # In: 28 x 28 x 1, depth = 1
-        # Out: 14 x 14 x 1, depth=64
-        input_shape = (self.img_rows, self.img_cols, self.channel)
-        self.discriminator.add(Conv2D(depth * 1, 5, strides=2, input_shape=input_shape,
-                                      padding='same', activation=LeakyReLU(alpha=0.2)))
+        self.discriminator.add(Conv2D(64, 5, strides=2,
+                                      input_shape=(*self.preprocessor.img_resize, self.img_channels),
+                                      padding='same'))
+        self.discriminator.add(LeakyReLU(alpha=0.2))
         self.discriminator.add(Dropout(dropout))
 
-        self.discriminator.add(Conv2D(depth * 2, 5, strides=2,
-                                      padding='same', activation=LeakyReLU(alpha=0.2)))
+        self.discriminator.add(Conv2D(128, 5, strides=2, padding='same'))
+        self.discriminator.add(LeakyReLU(alpha=0.2))
         self.discriminator.add(Dropout(dropout))
 
-        self.discriminator.add(Conv2D(depth * 4, 5, strides=2,
-                                      padding='same', activation=LeakyReLU(alpha=0.2)))
+        self.discriminator.add(Conv2D(256, 5, strides=2, padding='same'))
+        self.discriminator.add(LeakyReLU(alpha=0.2))
         self.discriminator.add(Dropout(dropout))
 
-        self.discriminator.add(Conv2D(depth * 8, 5, strides=1, padding='same',
-                                      activation=LeakyReLU(alpha=0.2)))
+        self.discriminator.add(Conv2D(512, 5, strides=1, padding='same'))
+        self.discriminator.add(LeakyReLU(alpha=0.2))
         self.discriminator.add(Dropout(dropout))
 
         # Out: 1-dim probability
         self.discriminator.add(Flatten())
-        self.discriminator.add(Dense(1))
-        self.discriminator.add(Activation('sigmoid'))
-        self.discriminator.summary()
+        self.discriminator.add(Dense(output_size, activation='sigmoid'))
+        self.generator.summary()
 
-    def add_generator(self):
+    def add_generator(self, z_vector=100):
         dropout = 0.4
-        depth = 64 + 64 + 64 + 64
-        dim = 7
+        depth = 64 * 4
+        initial_width = int(self.preprocessor.img_resize[0] / 4)
+        initial_height = int(self.preprocessor.img_resize[1] / 4)
+
         # In: 100
         # Out: dim x dim x depth
-        self.generator.add(Dense(dim * dim * depth, input_dim=100))
+        self.generator.add(Dense(initial_width * initial_height * depth, input_dim=z_vector))
         self.generator.add(BatchNormalization(momentum=0.9))
         self.generator.add(Activation('relu'))
-        self.generator.add(Reshape((dim, dim, depth)))
+        self.generator.add(Reshape((initial_width, initial_height, depth)))
         self.generator.add(Dropout(dropout))
+
         # In: dim x dim x depth
         # Out: 2*dim x 2*dim x depth/2
         self.generator.add(UpSampling2D())
         self.generator.add(Conv2DTranspose(int(depth / 2), 5, padding='same'))
         self.generator.add(BatchNormalization(momentum=0.9))
         self.generator.add(Activation('relu'))
+
         self.generator.add(UpSampling2D())
         self.generator.add(Conv2DTranspose(int(depth / 4), 5, padding='same'))
         self.generator.add(BatchNormalization(momentum=0.9))
         self.generator.add(Activation('relu'))
+
         self.generator.add(Conv2DTranspose(int(depth / 8), 5, padding='same'))
         self.generator.add(BatchNormalization(momentum=0.9))
         self.generator.add(Activation('relu'))
-        # Out: 28 x 28 x 1 grayscale image [0.0,1.0] per pix
-        self.generator.add(Conv2DTranspose(1, 5, padding='same'))
+
+        self.generator.add(Conv2DTranspose(self.img_channels, 5, padding='same'))
         self.generator.add(Activation('sigmoid'))
         self.generator.summary()
-        return self.G
 
-    def build_models(self):
-        optimizer = RMSprop(lr=0.0008, clipvalue=1.0, decay=6e-8)
-        self.discriminator_model.add(self.discriminator())
+    def add_discriminator_model(self):
+        optimizer = RMSprop(lr=0.0002, decay=6e-8)
+        self.discriminator_model.add(self.discriminator)
         self.discriminator_model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-        optimizer = RMSprop(lr=0.0004, clipvalue=1.0, decay=3e-8)
-        self.adversarial_model.add(self.generator())
-        self.adversarial_model.add(self.discriminator())
+    def add_adversarial_model(self):
+        optimizer = RMSprop(lr=0.0001, decay=3e-8)
+        self.adversarial_model = Sequential()
+        self.adversarial_model.add(self.generator)
+        self.adversarial_model.add(self.discriminator)
         self.adversarial_model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
     def train(self):
@@ -94,6 +104,8 @@ class Gans:
         x = np.concatenate((images_train, images_fake))
         y = np.ones([2 * batch_size, 1])
         y[batch_size:, :] = 0
+
+
         d_loss = self.discriminator.train_on_batch(x, y)
         y = np.ones([batch_size, 1])
         noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
--- tests/gans_tests.py
+++ tests/gans_tests.py
@@ -16,15 +16,14 @@ class TestGans:
 
     def test_gans(self):
         img_resize = None
-        validation_split_size = 0.2
         train_jpeg_dir, test_jpeg_dir, test_jpeg_additional, train_csv_file = data_helper.get_jpeg_data_files_paths()
         preprocessor = AmazonPreprocessor(train_jpeg_dir, train_csv_file, test_jpeg_dir, test_jpeg_additional,
-                                          img_resize, validation_split_size)
+                                          img_resize, validation_split=0.1)
         preprocessor.init()
-        print("Hello2")
         gans = Gans(preprocessor)
-        gans.add_discriminator()
+        gans.add_discriminator(17)
         gans.add_generator()
-        gans.build_models()
-        gans.train()
+        gans.add_discriminator_model()
+        gans.add_adversarial_model()
+        #gans.train()
         print("Dir", train_jpeg_dir)
